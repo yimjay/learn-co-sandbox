753890be0730783d6ab41f2a7dd75ae71f8e898e
Ji Yim <gsooyim@gmail.com>
2018-06-04 04:48:31 +0000

    automatically backed up by learn

---
+++ mapping-ruby-classes-lab/lib/song.rb
@@ -0,0 +1,22 @@
+    class Song
+     
+      attr_accessor :name, :album, :id
+     
+      def initialize(name, album, id=nil)
+        @id = id
+        @name = name
+        @album = album
+      end
+     
+      def self.create_table
+        sql =  <<-SQL 
+          CREATE TABLE IF NOT EXISTS songs (
+            id INTEGER PRIMARY KEY, 
+            name TEXT, 
+            album TEXT
+            )
+            SQL
+        DB[:conn].execute(sql) 
+      end
+     
+    end
